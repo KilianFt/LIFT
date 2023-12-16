63a423504f71f300b3dd9796d774b86082bc184a
Ran Wei <rw422@tamu.edu>
2023-12-16 10:02:39 -0600

    add code to train and eval teacher in different environments

--- lift/teacher.py
+++ lift/teacher.py
@@ -1,34 +1,47 @@
 import os
 from pathlib import Path
 
-import wandb
-import gymnasium as gym
+# import wandb
+# import gymnasium as gym
+import numpy as np
 from stable_baselines3 import TD3
 
 from lift.evaluation import evaluate_emg_policy
+from lift.evaluation import evaluate_teacher_policy
 
 
-def maybe_train_teacher(config):    
+def maybe_train_teacher(train_env, test_env, config, filename, test_eps=5):    
     base_dir = Path(__file__).resolve().parents[1]
-    teacher_filename = base_dir / 'models' / 'teacher.zip'
+    teacher_filename = base_dir / 'models' / filename
 
-    env = gym.make('FetchReachDense-v2', max_episode_steps=100)
+    # env = gym.make('FetchReachDense-v2', max_episode_steps=100)
+
+    print("filename", teacher_filename)
 
     if not os.path.exists(teacher_filename):
         print('Training teacher')
         os.makedirs(teacher_filename.parent, exist_ok=True)
 
-        teacher = train_teacher(env, config)
+        teacher = train_teacher(train_env, config)
         teacher.save(teacher_filename)
     else:
         print('Loading trained teacher')
-        teacher = TD3.load(teacher_filename, env=env)
+        teacher = TD3.load(teacher_filename, env=test_env)
+    
+    train_reward = evaluate_teacher_policy(train_env, teacher, is_teacher=True)
+    test_reward = np.mean([
+        evaluate_teacher_policy(test_env, teacher, is_teacher=True) for _ in range(test_eps)
+    ])
+    # wandb.log({'teacher_reward': train_reward})
+    print(f"Teacher train_reward {train_reward}")
+    print(f"Teacher test_reward {test_reward}")
 
-    mean_reward = evaluate_emg_policy(teacher.get_env(), teacher, is_teacher=True)
-    wandb.log({'teacher_reward': mean_reward})
-    print(f"Teacher reward {mean_reward}")
+    stats = {
+        "train_reward": train_reward,
+        "test_reward": test_reward,
+    }
 
-    return teacher
+    return teacher, stats
 
 
 def train_teacher(env, config):
