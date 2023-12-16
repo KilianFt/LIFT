329d580dfd59a9c5ec3551303659feb1b7c17c38
Ran Wei <rw422@tamu.edu>
2023-12-16 10:02:03 -0600

    add code to work with new gym version. need to be resolved later

--- lift/evaluation.py
+++ lift/evaluation.py
@@ -11,3 +11,19 @@ def evaluate_emg_policy(env, model, n_eval_steps=1000, is_teacher=False):
         observation, reward, terminated, info = env.step(action)
         rewards[i] = reward[-1]
     return rewards.mean()
+
+def evaluate_teacher_policy(env, model, n_eval_steps=1000, is_teacher=False):
+    actions = []
+    rewards = np.zeros(n_eval_steps)
+    observation, info = env.reset()
+    for i in range(n_eval_steps):
+        if is_teacher:
+            action, _ = model.predict(observation)
+        else:
+            action = model.sample_action(observation)
+        observation, reward, terminated, truncated, info = env.step(action)        
+        rewards[i] = reward
+        actions.append(action)
+    
+    actions = np.stack(actions)
+    return rewards.mean()
