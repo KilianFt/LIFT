61aec3e9613ad578374b71a19279a727fc9f1c86
KilianFt <kilian.freitag@diefreitags.de>
2023-12-18 16:18:35 +0100

    load mad dataset

--- lift/datasets.py
+++ lift/datasets.py
@@ -1,3 +1,7 @@
+import os
+import re
+
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
@@ -12,3 +16,44 @@ class EMGSLDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.obs[idx], self.action[idx]
+
+
+def get_mad_sample(data_path, emg_min = -128, emg_max = 127, filter_labels = False):
+    emg = []
+    labels = []
+    for data_file in os.listdir(data_path):
+        if data_file.endswith(".dat"):
+            label = int(re.findall(r'\d+', data_file)[0])
+            data_read_from_file = np.fromfile((data_path + '/' + data_file), dtype=np.int16)
+            data_read_from_file = np.array(data_read_from_file, dtype=np.float32)
+
+            example = None
+            emg_vector = []
+            for value in data_read_from_file:
+                emg_vector.append(value)
+                if (len(emg_vector) >= 8):
+                    if example is None:
+                        example = emg_vector
+                    else:
+                        example = np.row_stack((example, emg_vector))
+                    emg_vector = []
+
+            norm_emg = np.interp(example, (emg_min, emg_max), (-1, 1))
+
+            emg.append(norm_emg)
+            labels.append(label)
+
+    if filter_labels:
+        desired_labels = [0, 1, 2, 3, 4]
+
+        filtered_labels = []
+        filtered_emg = []
+        for i, label in enumerate(labels):
+            if label in desired_labels:
+                filtered_emg.append(emg[i])
+                filtered_labels.append(label)
+
+        emg = filtered_emg
+        labels = filtered_labels
+
+    return emg, labels
