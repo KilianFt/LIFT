8803c0e4406fba7a62d13813f6211428c4de6fb8
Ran Wei <rw422@tamu.edu>
2024-02-19 11:35:49 -0600

    update eval function to collect trajectory data

--- lift/evaluation.py
+++ lift/evaluation.py
@@ -1,8 +1,10 @@
-import numpy as  np
+import copy
+import numpy as np
 from gymnasium.utils.step_api_compatibility import step_api_compatibility
 
-def evaluate_emg_policy(env, model, n_eval_steps=1000, is_teacher=False):
-    rewards = np.zeros(n_eval_steps)
+def evaluate_emg_policy(env, model, n_eval_steps=1000, use_terminate=True, is_teacher=False, save_data=True):
+    data = {"obs": [], "act": [], "rwd": [], "next_obs": [], "done": []}
+
     observation = env.reset()
     if isinstance(observation, tuple):
         observation = observation[0]
@@ -12,12 +14,32 @@ def evaluate_emg_policy(env, model, n_eval_steps=1000, is_teacher=False):
             action, _ = model.predict(observation)
         else:
             action = model.sample_action(observation)
-        # observation, reward, terminated, info = env.step(action)
-        # rewards[i] = reward[-1]
         
-        observation, reward, terminated, info = step_api_compatibility(
+        next_observation, reward, terminated, info = step_api_compatibility(
             env.step(action), 
             output_truncation_bool=False
         )
-        rewards[i] = float(reward)
-    return rewards.mean()
+
+        data["rwd"].append(float(reward))
+        if save_data:
+            data["obs"].append(copy.deepcopy(observation))
+            data["act"].append(action)
+            data["next_obs"].append(copy.deepcopy(next_observation))
+            data["done"].append(terminated)
+        
+        if use_terminate and terminated:
+            break
+        observation = next_observation
+    
+    if save_data: 
+        if isinstance(data["obs"][0], dict):
+            keys = list(data["obs"][0].keys())
+            data["obs"] = {k: np.concatenate([o[k] for o in data["obs"]], axis=0) for k in keys}
+            data["next_obs"] = {k: np.concatenate([o[k] for o in data["next_obs"]], axis=0) for k in keys}
+        else:
+            data["obs"] = np.concatenate(data["obs"], axis=0)
+            data["next_obs"] = np.concatenate(data["next_obs"], axis=0)
+        data["act"] = np.concatenate(data["act"], axis=0)
+        data["rwd"] = np.stack(data["rwd"])
+        data["done"] = np.concatenate(data["done"], axis=0)
+    return data
