e638c3af72354564a621af13cf70ea576097878d
Ran Wei <rw422@tamu.edu>
2024-05-13 22:34:22 -0500

    make conditioned teacher and add to emg env

--- lift/teacher.py
+++ lift/teacher.py
@@ -1,6 +1,28 @@
+import copy
+import numpy as np
 from lift.rl.sac import SAC
 from lift.rl.sac_meta import MetaSAC
 from lift.rl.utils import parallel_env_maker
+
+class ConditionedTeacher:
+    """Wrapper to reset teacher with random meta variables for simulated trajectories"""
+    def __init__(self, teacher, noise_range=0., alpha_range=0.):
+        self.noise_range = noise_range
+        self.alpha_range = alpha_range
+        self.teacher = teacher
+
+    def reset(self):
+        noise = np.random.uniform(self.noise_range[0], self.noise_range[1])
+        # alpha = np.random.uniform(self.alpha_range[0], self.alpha_range[1])
+        # self.meta_vars = np.array([noise, alpha], dtype=self.observation_space["observation"].dtype)
+        self.meta_vars = np.array([noise])
+    
+    def sample_action(self, obs, sample_mean=False):
+        obs_ = copy.deepcopy(obs)
+        # obs_["observation"][..., -2] = self.noise
+        # obs_["observation"][..., -1] = self.alpha
+        obs_["observation"][..., -1] = self.meta_vars
+        return self.teacher.sample_action(obs_, sample_mean)
 
 
 def load_teacher(config, load_frozen=True, meta=False):
